e8527f7ab928163abd1687a78df47dfda5c4b85c
ZhangDi <demonzhang1990@gmail.com>
2018-06-13 14:10:24 +0800

    模块

---
+++ StudyNote/p3.6_6模块/hello.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#Filename:hello.py
+
+
+def  print_func(par):
+	print('hello:', par)
+	return
+
+print_func('老大')
---
+++ StudyNote/p3.6_6模块/test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+#Filename:test.py
+
+#导入模块
+import hello
+
+hello.print_func('猪婆')
